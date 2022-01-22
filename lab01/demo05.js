d025c656c3f59941a8dfa40da99ee694891d8eef
Tram Pham <phtram.2021.21850031@gmail.com>
2022-01-22 13:53:51 +0700

    feature(lab01): ES - Operator

---
+++ lab01/demo05.js
@@ -0,0 +1,17 @@
+/**
+ * Các phép toán và phép so sánh
+ */
+let num = 100;
+console.log("num = num + 1 =>", num + 1);
+console.log("num = num - 1 =>", num - 1);
+console.log("num = num * 2 =>", num * 2);
+console.log("num = num / 3 =>", num / 3); //Chia hết
+console.log("num = num % 3 =>", num % 3); //Chia lấy dư
+console.log("num = num / 3 =>", parseInt(num / 3)); //Chia lấy nguyên
+
+console.log("num =", num);
+console.log("num++ =>", num++);
+console.log("numm =", num);
+console.log("++num =>", ++num);
+console.log("numm =", num);
+console.log("--num =>", --num);
