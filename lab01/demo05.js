f846fcd141173cbbb9bc6ec194d8d4b7212c58b0
Tram Pham <phtram.2021.21850031@gmail.com>
2022-01-22 13:54:40 +0700

    refactor(lab01): ES - Operator

--- lab01/demo05.js
+++ lab01/demo05.js
@@ -9,9 +9,20 @@ console.log("num = num / 3 =>", num / 3); //Chia hết
 console.log("num = num % 3 =>", num % 3); //Chia lấy dư
 console.log("num = num / 3 =>", parseInt(num / 3)); //Chia lấy nguyên
 
+num = 100;
 console.log("num =", num);
 console.log("num++ =>", num++);
 console.log("numm =", num);
 console.log("++num =>", ++num);
 console.log("numm =", num);
 console.log("--num =>", --num);
+
+num = 100;
+num += 5;
+console.log("num += 5 =>", num);
+num -= 4;
+console.log("num -= 4 =>", num);
+num *= 2;
+console.log("num *= 2 =>", num);
+num /= 6;
+console.log("num /= 5 =>", num);
